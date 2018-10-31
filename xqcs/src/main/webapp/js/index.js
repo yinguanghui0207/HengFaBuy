09087280d66952b6d7e52c63af2cd190f28e9560
YinGuanghui <767475914@qq.com>
2018-10-31 17:43:24 +0800

    添加首页

--- xqcs/src/main/webapp/js/index.js
+++ xqcs/src/main/webapp/js/index.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
 	$(window).scroll(function(e){
@@ -14,5 +15,63 @@ $(document).ready(function() {
 		// }else{
 
 		// }
-	})
+	});
+
+	$('#submit').click(function(){
+		var name=$('#name').val();
+		var phone=$('#phone').val();
+		var city=$('#city').val();
+		var msg=$('#messages').val();
+		if(isEmpty(name)){
+			console.log('姓名不能为空');
+			showMsg('姓名不能为空');
+			return;
+		};
+		if(isEmpty(phone)){
+			console.log('电话不能为空');
+			showMsg('电话不能为空');
+			return;
+		};
+		if(isEmpty(city)){
+			console.log('地址不能为空');
+			showMsg('地址不能为空');
+			return;
+		};
+		$.ajax({
+			type: "POST",
+			url: "/pc/index",
+			data: {name:name, phone:phone,city:city,message:msg},
+			dataType: "json",
+			success: function(data){
+				showMsg('留言成功！');
+				$('#name,#phone,#city,#messages').val('');   //清空input里面的所有内容
+			},
+			err:function(err){
+				showMsg('留言失败，请检查网络后重试。');
+				console.log(err);
+			}
+		});
+	});
+	if($('#txtbox')){
+		$('#txtbox').myScroll({
+			speed: 30, //数值越大，速度越慢
+			rowHeight: 40 //li的高度
+		});
+	}
+	
 });
+
+function isEmpty(txt){
+	return txt.match(/^\s*$/);
+};
+
+function showMsg(msg){
+	$('#msg').text(msg);
+	$('#alert').fadeIn();
+}
+
+$('#cancel').click(function(){
+	$('#alert').fadeOut();
+});
+
+
